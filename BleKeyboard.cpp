850727a8d51aff8e89cd5524308877b7b46423df
Mustafa Tanrıverdi <78870676+5mustafa@users.noreply.github.com>
2024-08-07 17:43:58 +0300

    Update BleKeyboard.cpp

--- BleKeyboard.cpp
+++ BleKeyboard.cpp
@@ -127,13 +127,14 @@ void BleKeyboard::begin(void)
 #else
 
   BLESecurity* pSecurity = new BLESecurity();
-   if (CONFIG_IDF_TARGET_ESP32C3 || CONFIG_IDF_TARGET_ESP32S3)
-  {
-	pSecurity->setAuthenticationMode(ESP_LE_AUTH_BOND);
-  }
-  else{
-	pSecurity->setAuthenticationMode(ESP_LE_AUTH_REQ_SC_MITM_BOND);
-  }
+ 
+ //eğer kart esp32s3 veya esp32c3 ise bu kod bloğu çalışır
+#if defined(CONFIG_IDF_TARGET_ESP32S3) || defined(CONFIG_IDF_TARGET_ESP32C3)
+  pSecurity->setAuthenticationMode(ESP_LE_AUTH_BOND);
+#else
+  pSecurity->setAuthenticationMode(ESP_LE_AUTH_BOND | ESP_LE_AUTH_REQ_SC_MITM_BOND);
+#endif
+
 
 #endif // USE_NIMBLE
 
