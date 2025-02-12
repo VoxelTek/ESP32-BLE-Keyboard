a3a8c5cf0b2d2181d6862a741242e59148bc0f32
VoxelTek <53562267+VoxelTek@users.noreply.github.com>
2025-02-12 16:20:26 +1000

    Remove "advertising" functions from BleKeyboard.h

--- BleKeyboard.h
+++ BleKeyboard.h
@@ -150,8 +150,6 @@ public:
   BleKeyboard(String deviceName = "ESP32 Keyboard", String deviceManufacturer = "Espressif", uint8_t batteryLevel = 100);
   void begin(void);
   void end(void);
-  void startAdvertising(void);
-  void stopAdvertising(void);
   void sendReport(KeyReport* keys);
   void sendReport(MediaKeyReport* keys);
   size_t press(uint8_t k);
